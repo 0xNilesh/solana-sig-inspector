132fd8e77dfaed357feac4c44e407ab7e4d413cf
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-04-14 19:08:26 +0530

    feat: added node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -8,15 +9,12 @@ export default defineConfig(({ mode }) => ({
     host: "::",
   },
   plugins: [
-    react()
+    react(),
+    nodePolyfills()
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      buffer: path.resolve(__dirname, 'node_modules', 'buffer'),
     },
-  },
-  define: {
-    'global.Buffer': 'buffer.Buffer',
   },
 }));
